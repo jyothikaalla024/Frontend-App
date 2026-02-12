e664ad1e4f18ae4ab87c226d60c0c6b0753bdba8
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-02-12 19:39:54 +0530

    Refactor API calls with error handling
    Updated API base URL and added error handling for user and order fetch functions.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,51 @@
-const API_BASE_URL = "http://localhost:8080"; // API Gateway / ALB URL later
 
+ 
+const API_BASE_URL = "https://api.amznpro.online";
+ 
 async function loadUsers() {
-  const res = await fetch(`${API_BASE_URL}/users`);
-  const data = await res.json();
-  document.getElementById("result").innerText =
-    JSON.stringify(data, null, 2);
-}
 
+  try {
+
+    const res = await fetch(`${API_BASE_URL}/users`);
+
+    if (!res.ok) throw new Error("Failed to fetch users");
+
+    const data = await res.json();
+
+    document.getElementById("result").innerText =
+
+      JSON.stringify(data, null, 2);
+
+  } catch (error) {
+
+    document.getElementById("result").innerText =
+
+      "Error: " + error.message;
+
+  }
+
+}
+ 
 async function loadOrders() {
-  const res = await fetch(`${API_BASE_URL}/orders`);
-  const data = await res.json();
-  document.getElementById("result").innerText =
-    JSON.stringify(data, null, 2);
+
+  try {
+
+    const res = await fetch(`${API_BASE_URL}/orders`);
+
+    if (!res.ok) throw new Error("Failed to fetch orders");
+
+    const data = await res.json();
+
+    document.getElementById("result").innerText =
+
+      JSON.stringify(data, null, 2);
+
+  } catch (error) {
+
+    document.getElementById("result").innerText =
+
+      "Error: " + error.message;
+
+  }
+
 }
