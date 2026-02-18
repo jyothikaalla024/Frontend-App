1bc6d3b5be10743c0fca2b6a707f4e5d260e5daa
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-02-18 07:16:42 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:4000"; 
+const API_BASE_URL = "https://localhost:4000"; 
 // Change to https://mbry.digital when deployed
  
 // ================= REGISTER =================
